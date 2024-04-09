0793e1a29392e165b2da2c4e2c6937075fedcdaa
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2024-04-09 13:18:29 +0200

    Display a calendar to choose a meeting date and time

--- lib/calendlex_web/live/components/event_type.ex
+++ lib/calendlex_web/live/components/event_type.ex
@@ -12,4 +12,62 @@ defmodule CalendlexWeb.Components.EventType do
     </.link>
     """
   end
+
+  def calendar(assigns) do
+    %{
+      current_path: current_path,
+      previous_month: previous_month,
+      next_month: next_month
+    } = assigns
+
+    previous_month_path = build_path(current_path, %{month: previous_month})
+    next_month_path = build_path(current_path, %{month: next_month})
+
+    assigns =
+      assigns
+      |> assign(previous_month_path: previous_month_path)
+      |> assign(next_month_path: next_month_path)
+
+    ~H"""
+    <div>
+      <div class="flex items-center mb-8">
+        <div class="flex-1">
+          <%= Timex.format!(@current, "{Mshort} {YYYY}") %>
+        </div>
+        <div class="flex justify-end flex-1 text-right">
+          <%= live_patch to: @previous_month_path do %>
+            <button class="flex items-center justify-center w-10 h-10 text-blue-700 align-middle rounded-full hover:bg-blue-200">
+              <i class="fas fa-chevron-left"></i>
+            </button>
+          <% end %>
+          <%= live_patch to: @next_month_path do %>
+            <button class="flex items-center justify-center w-10 h-10 text-blue-700 align-middle rounded-full hover:bg-blue-200">
+              <i class="fas fa-chevron-right"></i>
+            </button>
+          <% end %>
+        </div>
+      </div>
+      <div class="mb-6 text-center uppercase calendar grid grid-cols-7 gap-y-2 gap-x-2">
+        <div class="text-xs">Mon</div>
+        <div class="text-xs">Tue</div>
+        <div class="text-xs">Wed</div>
+        <div class="text-xs">Thu</div>
+        <div class="text-xs">Fri</div>
+        <div class="text-xs">Sat</div>
+        <div class="text-xs">Sun</div>
+      </div>
+      <div class="flex items-center gap-x-1">
+        <i class="fas fa-globe-americas"></i>
+        <%= @time_zone %>
+      </div>
+    </div>
+    """
+  end
+
+  defp build_path(current_path, params) do
+    current_path
+    |> URI.parse()
+    |> Map.put(:query, URI.encode_query(params))
+    |> URI.to_string()
+  end
 end
--- lib/calendlex_web/live/event_type_live.ex
+++ lib/calendlex_web/live/event_type_live.ex
@@ -1,6 +1,9 @@
 defmodule CalendlexWeb.EventTypeLive do
   use CalendlexWeb, :live_view
 
+  alias CalendlexWeb.Components.EventType
+  alias Timex.Duration
+
   def mount(%{"event_type_slug" => slug} = params, _session, socket) do
     case Calendlex.get_event_type_by_slug(slug) do
       {:ok, event_type} ->
@@ -8,11 +11,39 @@ defmodule CalendlexWeb.EventTypeLive do
           socket
           |> assign(event_type: event_type)
           |> assign(page_title: event_type.name)
+          |> assign_dates()
 
         {:ok, socket}
 
       {:error, :not_found} ->
         {:ok, socket, layout: {CalendlexWeb.Layouts, :not_found}}
     end
+  end
+
+  defp assign_dates(socket) do
+    current = Timex.today(socket.assigns.time_zone)
+    beginning_of_month = Timex.beginning_of_month(current)
+    end_of_month = Timex.end_of_month(current)
+
+    previous_month =
+      beginning_of_month
+      |> Timex.add(Duration.from_days(-1))
+      |> date_to_month()
+
+    next_month =
+      end_of_month
+      |> Timex.add(Duration.from_days(1))
+      |> date_to_month()
+
+    socket
+    |> assign(current: current)
+    |> assign(beginning_of_month: beginning_of_month)
+    |> assign(end_of_month: end_of_month)
+    |> assign(previous_month: previous_month)
+    |> assign(next_month: next_month)
+  end
+
+  defp date_to_month(date_time) do
+    Timex.format!(date_time, "{YYYY}-{0M}")
   end
 end
