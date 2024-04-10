193702a163a77ad205321f224da811c32116a212
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2024-04-10 13:11:07 +0200

    Book a time slot

--- lib/calendlex_web/live/live_view_helpers.ex
+++ lib/calendlex_web/live/live_view_helpers.ex
@@ -5,4 +5,23 @@ defmodule CalendlexWeb.LiveViewHelpers do
     |> Enum.map(&elem(&1, 0))
     |> Enum.join(" ")
   end
+
+  def schedule_string(start_at, end_at, time_zone) do
+    slot_start_str =
+      start_at
+      |> DateTime.shift_zone!(time_zone)
+      |> Timex.format!("{h24}:{m}")
+
+    slot_end_str =
+      end_at
+      |> DateTime.shift_zone!(time_zone)
+      |> Timex.format!("{h24}:{m}")
+
+    date_str =
+      start_at
+      |> DateTime.shift_zone!(time_zone)
+      |> Timex.format!("{WDfull}, {Mfull} {D}, {YYYY}")
+
+    "#{slot_start_str} - #{slot_end_str}, #{date_str}"
+  end
 end
--- lib/calendlex_web/live/schedule_event_live.ex
+++ lib/calendlex_web/live/schedule_event_live.ex
@@ -1,7 +1,27 @@
 defmodule CalendlexWeb.ScheduleEventLive do
   use CalendlexWeb, :live_view
 
-  def mount(_params, _session, socket) do
-    {:ok, socket}
+  alias Calendlex.Event
+
+  def mount(params, _session, socket) do
+    %{"event_type_slug" => slug, "time_slot" => time_slot} = params
+
+    with {:ok, event_type} <- Calendlex.get_event_type_by_slug(slug),
+         {:ok, start_at, _} <- DateTime.from_iso8601(time_slot) do
+      end_at = Timex.add(start_at, Timex.Duration.from_minutes(event_type.duration))
+      changeset = Event.changeset(%Event{}, %{})
+
+      socket =
+        socket
+        |> assign(changeset: changeset)
+        |> assign(end_at: end_at)
+        |> assign(event_type: event_type)
+        |> assign(start_at: start_at)
+
+      {:ok, socket}
+    else
+      _ ->
+        {:ok, socket, layout: {CalendlexWeb.Layouts, :not_found}}
+    end
   end
 end
